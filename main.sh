338ec7b9e9b1b86e5232cb940fbb2dadcc922608
João Guilherme M. Martins <particular.guilherme.martins@gmail.com>
2023-12-11 10:48:50 -0300

    correcao joao-martins.sh e update main.sh

--- main.sh
+++ main.sh
@@ -3,3 +3,9 @@ nome_do_grupo="NomeDoGrupo"
 
 echo "Nome do grupo: $nome_do_grupo"
 echo "Alunos: João Guilherme Milian Martins, Matheus Marcelino, Ramon, Breno, Caio, Thiago"
+source joao-martins.sh
+if [ -d Joao-Martins ]; then
+    echo "Pasta disponível"
+else
+    echo "Pasta não disponível"
+fi
